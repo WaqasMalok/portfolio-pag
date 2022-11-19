59b9185add2dbe9a62adb594f4e639e453451083
WaqasMalok <waqasimalok@gmail.com>
2022-11-20 01:38:16 +0500

    first

---
+++ new.js
@@ -0,0 +1,19 @@
+function show_menu() {
+    var a = document.querySelector("#nav-mb");
+    a.style.display = "block";
+    var b = document.querySelector("#show");
+    b.style.display = "none";
+    var c = document.querySelector("#hide");
+    c.style.display = "block";
+
+}
+
+function hide_menu() {
+    var a = document.querySelector("#nav-mb");
+    a.style.display = "none";
+    var b = document.querySelector("#show");
+    b.style.display = "block";
+    var c = document.querySelector("#hide");
+    c.style.display = "none";
+
+}
